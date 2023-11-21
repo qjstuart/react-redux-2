c5b7ec7d2669d3dd37073ad7a19a9b3caf1095f4
qjstuart <quentinqjs@gmail.com>
2023-11-21 18:20:04 +0100

    Begin porting sideEffects from component to thunks

--- src/App.js
+++ src/App.js
@@ -22,42 +22,42 @@ function App() {
         return;
       }
 
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending Data",
-          message: "Sending cart data...",
-        })
-      );
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "pending",
+      //     title: "Sending Data",
+      //     message: "Sending cart data...",
+      //   })
+      // );
 
-      const response = await fetch(
-        "https://react-http-9bf27-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
+      // const response = await fetch(
+      //   "https://react-http-9bf27-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      //   { method: "PUT", body: JSON.stringify(cart) }
+      // );
 
-      if (!response.ok) {
-        throw new Error("Fetch: Sending cart data failed, response not OK.");
-      }
+      // if (!response.ok) {
+      //   throw new Error("Fetch: Sending cart data failed, response not OK.");
+      // }
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Data Sent",
-          message: "Cart data sent successfully...",
-        })
-      );
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "success",
+      //     title: "Data Sent",
+      //     message: "Cart data sent successfully...",
+      //   })
+      // );
     }
 
     try {
       sendCartData();
     } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Data Send Error",
-          message: "Failed to send cart data...",
-        })
-      );
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "error",
+      //     title: "Data Send Error",
+      //     message: "Failed to send cart data...",
+      //   })
+      // );
     }
   }, [cart, dispatch]);
 
