67ed59e46b66595828062121f393c59b3398dba2
qjstuart <quentinqjs@gmail.com>
2023-11-23 11:57:15 +0100

    Remove unnecessary import

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 import uiSlice from "./ui-slice";
 import cartSlice from "./cart-slice";
